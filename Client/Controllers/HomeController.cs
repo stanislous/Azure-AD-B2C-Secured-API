a83239eadb45ee770d673e8f4f3c4579f2f919dd
Shehan Vanderputt <shehan.v@dubliw.com>
2024-03-05 13:50:20 +0530

    Add project files.

---
+++ Client/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web;
+using WebApp_OpenIDConnect_DotNet.Models;
+
+namespace TodoListClient.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ITokenAcquisition tokenAcquisition;
+
+        public HomeController(ITokenAcquisition tokenAcquisition)
+        {
+            this.tokenAcquisition = tokenAcquisition;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
